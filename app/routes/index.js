c07df65d1d0b7fc076186db452a64bd3500cae19
Diego Novais <contato@diegonovais.com.br>
2018-01-03 15:38:52 -0200

    Adjustes and initial commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,23 @@
 var api = require('../api'),
-    path = require('path');
+  path = require('path');
 
-module.exports  = function(app) {
-    
-    app.route('/v1/fotos')
-        .post(api.adiciona)
-        .get(api.lista);
+module.exports = function (app) {
 
-    app.route('/v1/fotos/:fotoId')
-        .delete(api.remove)
-        .get(api.busca)
-        .put(api.atualiza);
+  app.route('/v1/fotos')
+    .post(api.adiciona)
+    .get(api.lista);
 
-    app.get('/v1/fotos/grupo/:grupoId', api.listaPorGrupo);
-        
+  app.route('/v1/fotos/:fotoId')
+    .delete(api.remove)
+    .get(api.busca)
+    .put(api.atualiza);
 
-    // habilitando HTML5MODE
-    app.all('/*', function(req, res) {
-        res.sendFile(path.resolve('public/index.html'));
-    });
+  app.get('/v1/grupos', api.listaGrupos)
+  app.get('/v1/fotos/grupo/:grupoId', api.listaPorGrupo);
+
+
+  // habilitando HTML5MODE
+  app.all('/*', function (req, res) {
+    res.sendFile(path.resolve('public/index.html'));
+  });
 };
--- config/express.js
+++ config/express.js
@@ -1,10 +1,10 @@
-var express = require('express')
-    ,app = express()
-    ,bodyParser = require('body-parser')
-    ,routes = require('../app/routes');
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  routes = require('../app/routes');
 
 app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 routes(app);
