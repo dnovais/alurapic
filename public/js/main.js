91d89c660de53007b6b1ef83e7e37dcf7695e8e9
Diego Novais <contato@diegonovais.com.br>
2018-01-09 20:45:02 -0200

    aula 06

--- public/js/main.js
+++ public/js/main.js
@@ -6,4 +6,34 @@
   diretivas.
 
 */
-angular.module('alurapic', ['Directives', 'ngAnimate']);
+angular.module('alurapic', ['Directives', 'ngAnimate', 'ngRoute'])
+.config(function($routeProvider, $locationProvider){
+  /* 
+    Para deixar a url mais elegante sem o /#/, ativamos o html5mode, assim o angular deixa de
+    trabalhar com # e passa a trabalhar normal, mas um detalhe importante precisa ser levado
+    em consideração: o backend tb precisa estar preparado para isso.
+
+    Outra coisa que será necessário, sera colocar tag base -> href = '/' no public/index.html.
+    Se o navegador n suportar não tiver suporte, ele volta ao normal /#/...
+  */
+
+  $locationProvider.html5Mode(true);
+  
+  /*
+    Quando a url tiver /#/photos ou /photos, o Angular entende qual o template e controller ele ira
+    carregar na requisicao.
+
+    Sendo assim, as partials da view serao injetadas dentro do ng-view
+  */
+  
+  $routeProvider.when('/photos', {
+    templateUrl: 'partials/main.html',
+    controller: 'PhotosController'
+  });
+
+  $routeProvider.when('/photos/new', {
+    templateUrl: 'partials/photos.html'
+  });
+
+  $routeProvider.otherwise({ redirectTo: '/photos'});
+});
